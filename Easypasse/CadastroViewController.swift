9f4c2bc7934bae8410159b35d89c552c8dc46e65
Gustavo Severo <gustavo.msevero@gmail.com>
2018-07-18 12:01:16 -0300

    Conseguindo pegar dados individuais do JSON vinda da API, tanto no cadastro quando no login

--- Easypasse/CadastroViewController.swift
+++ Easypasse/CadastroViewController.swift
@@ -21,15 +21,13 @@ class CadastroViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        //let context = appDelegate.persistentContainer.viewContext
-
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: false)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -40,64 +38,53 @@ class CadastroViewController: UIViewController {
         let nomeUsuario = self.campoUsuario.text;
         let cpf = self.campoCPF.text;
         let senha = self.campoSenha.text;
+        let plat = "ios"
+        let method = "app-set-usuario"
         
-        let param = ["nome": nomeUsuario, "cpf": cpf, "senha": senha, "method": "app-set-usuario"]
+        let param = ["nome": nomeUsuario, "cpf": cpf, "senha": senha, "method": method, "plataforma": plat]
         var _: HTTPHeaders = ["Content-Type": "application/json"]
         let url = "http://easypasse.com.br/gestao/wsCadastrar.php"
         
-        Alamofire.request(url, method:.post, parameters:param,encoding: JSONEncoding.default).responseJSON { response in
+        Alamofire.request(url, method:.post, parameters:param, encoding: JSONEncoding.default).responseJSON { response in
             switch response.result {
             case .success:
                 //print(response)
-                let json = JSON(response.result.value as Any)
-                let idusuario =  json["usuario"].arrayValue.map({$0["idUsuario"].stringValue})
-                let nome =  json["usuario"].arrayValue.map({$0["nome"].stringValue})
-                let cpf =  json["usuario"].arrayValue.map({$0["cpf"].stringValue})
-
-                print(idusuario)
-                print(nome)
-                print(cpf)
-
+                if let json = response.result.value as? [String: Any],
+                    let registro = json["registro"] as? [String: Any],
+                    let usuario = registro["usuario"] as? [String: Any],
+                    let nome = usuario["nome"] as? String {
+                    let cpf = usuario["cpf"] as? String
+                    let idusuario = usuario["idUsuario"] as? String
+                    
+                    let nomeUsuario: String = nome
+                    let cpfUsuario: String = cpf!
+                    let idUsuario: String = idusuario!
+                    
+                    
+                    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                    let context = appDelegate.persistentContainer.viewContext
+                    //let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+                    
+                    let dataUsuario = NSEntityDescription.insertNewObject(forEntityName: "Usuario", into: context)
+                    dataUsuario.setValue(nomeUsuario, forKey: "nome")
+                    dataUsuario.setValue(idUsuario, forKey: "idusuario")
+                    dataUsuario.setValue(cpfUsuario, forKey: "cpf")
+                    
+                    //Salvar os dados
+                    do {
+                        try context.save()
+                        print("Os dados foram salvos corretamente")
+                    } catch  {
+                        print("Erro ao salvar os dados")
+                    }
+                    
+                }
+                
             case .failure(let error):
                 print(0,"Error")
             }
         }
         
-//        Alamofire.request(url).validate().responseArray(keyPath: "usuario") { (response: DataResponse<[dadosUsuario]>) in
-//            UIApplication.shared.isNetworkActivityIndicatorVisible = false
-//            switch response.result {
-//            case .success:
-//                let usuarios = response.result.value ?? []
-//                for usuario in usuarios {
-//                    print(usuario.nome)
-//                }
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-        
-//        usuario =     (
-//            {
-//                ativo = 1;
-//                cpf = 49516715141;
-//                ddd = 000;
-//                dtnascimento = "<null>";
-//                email = "";
-//                empresa = 0;
-//                idGenero = 1;
-//                idPerfil = 3;
-//                idUsuario = 69;
-//                idUsuarioAdm = 12;
-//                logado = 1;
-//                nome = "SERGIO AFONSO";
-//                senha = d9bae3215f4677ddf2fa9972e0bf1c00;
-//                telefone = 0;
-//                username = "";
-//            }
-//        );
-
-        
-
     }
     
 }
--- Easypasse/ViewController.swift
+++ Easypasse/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import CoreData
+import SwiftyJSON
+import Alamofire
+import AlamofireObjectMapper
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var campoCPF: UITextField!
     @IBOutlet weak var campoSenha: UITextField!
     
@@ -21,15 +25,43 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func botaoAcesse(_ sender: Any) {
+        
+        let cpf = self.campoCPF.text
+        let senha = self.campoSenha.text
+        let plat = "ios"
+        
+        let param = ["cpf": cpf, "senha": senha, "method": "app-get-login", "plataforma": plat]
+        var _: HTTPHeaders = ["Content-Type": "application/json"]
+        let url = "http://easypasse.com.br/gestao/wsLogin.php"
+        
+        Alamofire.request(url, method:.post, parameters:param,encoding: JSONEncoding.default).responseJSON { response in
+            switch response.result {
+            case .success:
+                print(response)
+                //let nome = response.registro.usuario.nome as? String tentar quando puder
+                if let json = response.result.value as? [String: Any],
+                    let registro = json["registro"] as? [String: Any],
+                    let usuario = registro["usuario"] as? [String: Any],
+                    let nome = usuario["nome"] as? String {
+                    //print(nome)
+                    
+                }
+                
+            case .failure(let error):
+                print(0,"Error")
+            }
+        }
     }
-
     
 }
+
+
+
 
